5485c0fcf95be8a9df603151ec05b471f8740aa6
hgh <739041588@qq.com>
2019-05-11 15:47:40 +0800

    添加主页

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/CSS/reset.css'
+
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+new Vue({
+    router,
+    store,
+    render: function (h) {
+        return h(App)
+    }
+}).$mount('#app')
+
+// refresh()
+// window.onresize = function () {
+//   setTimeout(function () {
+//     refresh()
+//   }, 10)
+// }
+//
+// function refresh() {
+//   let deviceWidth = document.documentElement.clientWidth;
+//   if (deviceWidth > 1200) {
+//     document.documentElement.style.fontSize = 1200 / 12 + "px"
+//   } else {
+//     document.documentElement.style.fontSize = deviceWidth / 7.5 + "px"
+//   }
+// }
