f66e94839a5255ce1237ee5bc751da877383bcee
lsn <642250063@qq.com>
2019-05-11 17:38:45 +0800

    登录页

--- src/components/view.vue
+++ src/components/view.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
-
+        <div class="main">
+            <div>111</div>
+        </div>
+        <div>111</div>
     </div>
 </template>
 
 <script>
+    // var echarts = require('echarts/lib/echarts');   //引入模块
+    // require('echarts/lib/chart/bar');        //引入形状图
+    // var myChart = echarts.init(document.getElementById('main'));//生成图表
+
+
+
     export default {
         name: "view"
     }
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/components/login'
 import index from '@/components/index'
+import view from '@/components/view'
 
 Vue.use(Router)
 
@@ -9,7 +10,9 @@ export default new Router({
     routes: [
         {path: '/', name: 'login', component: login},
         {path: '*', redirect: '/'},
-        {path: '/index', name: 'index', component: index}
+        {path: '/index', name: 'index', component: index},
+        {path: '/view', name: 'view', component: view}
+
     ],
     mode: 'history'
 })
