93bd987b24bb790b3a8fa4a88120f9c0a5817063
lsn <642250063@qq.com>
2019-05-13 11:01:03 +0800

    登录页

--- src/components/view/long.vue
+++ src/components/view/long.vue
@@ -8,11 +8,6 @@
 <script>
     export default {
         name: "long",
-        data () {
-            return {
-                msg: 'Welcome to Your Vue.js App'
-            }
-        },
         mounted(){
             this.drawLine();
         },
@@ -22,17 +17,69 @@
                 let myChart = this.$echarts.init(document.getElementById('myChart'))
                 // 绘制图表
                 myChart.setOption({
-                    title: { text: '在Vue中使用echarts' },
-                    tooltip: {},
-                    xAxis: {
-                        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+                    tooltip : {
+                        trigger: 'axis',
+                        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                        }
                     },
-                    yAxis: {},
-                    series: [{
-                        name: '销量',
-                        type: 'bar',
-                        data: [5, 20, 36, 10, 10, 20]
-                    }]
+                    legend: {
+                        data:['利润', '支出', '收入']
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis : [
+                        {
+                            type : 'value'
+                        }
+                    ],
+                    yAxis : [
+                        {
+                            type : 'category',
+                            axisTick : {show: false},
+                            data : ['周一','周二','周三','周四','周五','周六','周日']
+                        }
+                    ],
+                    series : [
+                        {
+                            name:'利润',
+                            type:'bar',
+                            label: {
+                                normal: {
+                                    show: true,
+                                    position: 'inside'
+                                }
+                            },
+                            data:[200, 170, 240, 244, 200, 220, 210]
+                        },
+                        {
+                            name:'收入',
+                            type:'bar',
+                            stack: '总量',
+                            label: {
+                                normal: {
+                                    show: true
+                                }
+                            },
+                            data:[320, 302, 341, 374, 390, 450, 420]
+                        },
+                        {
+                            name:'支出',
+                            type:'bar',
+                            stack: '总量',
+                            label: {
+                                normal: {
+                                    show: true,
+                                    position: 'left'
+                                }
+                            },
+                            data:[-120, -132, -101, -134, -190, -230, -210]
+                        }
+                    ]
                 });
             }
         }
--- src/components/view/post.vue
+++ src/components/view/post.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div id="myChar" :style="{width: '300px', height: '300px'}">
+        <div id="myChar" :style="{width: '600px', height: '600px'}">
         </div>
     </div>
 </template>
@@ -18,21 +18,103 @@
         },
         methods: {
             drawLine(){
+                var dataAxis = ['点', '击', '柱', '子', '或', '者', '两', '指', '在', '触', '屏', '上', '滑', '动', '能', '够', '自', '动', '缩', '放'];
+                var data = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220];
+                var yMax = 500;
+                var dataShadow = [];
+
+                for (var i = 0; i < data.length; i++) {
+                    dataShadow.push(yMax);
+                }
                 // 基于准备好的dom，初始化echarts实例
                 let myChart = this.$echarts.init(document.getElementById('myChar'))
                 // 绘制图表
                 myChart.setOption({
-                    title: { text: '在Vue中使用echarts' },
-                    tooltip: {},
-                    xAxis: {
-                        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+                    title: {
+                        text: '特性示例：渐变色 阴影 点击缩放',
+                        subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
                     },
-                    yAxis: {},
-                    series: [{
-                        name: '销量',
-                        type: 'bar',
-                        data: [5, 20, 36, 10, 10, 20]
-                    }]
+                    xAxis: {
+                        data: dataAxis,
+                        axisLabel: {
+                            inside: true,
+                            textStyle: {
+                                color: '#fff'
+                            }
+                        },
+                        axisTick: {
+                            show: false
+                        },
+                        axisLine: {
+                            show: false
+                        },
+                        z: 10
+                    },
+                    yAxis: {
+                        axisLine: {
+                            show: false
+                        },
+                        axisTick: {
+                            show: false
+                        },
+                        axisLabel: {
+                            textStyle: {
+                                color: '#999'
+                            }
+                        }
+                    },
+                    dataZoom: [
+                        {
+                            type: 'inside'
+                        }
+                    ],
+                    series: [
+                        { // For shadow
+                            type: 'bar',
+                            itemStyle: {
+                                normal: {color: 'rgba(0,0,0,0.05)'}
+                            },
+                            barGap:'-100%',
+                            barCategoryGap:'40%',
+                            data: dataShadow,
+                            animation: false
+                        },
+                        {
+                            type: 'bar',
+                            itemStyle: {
+                                normal: {
+                                    color: new this.$echarts.graphic.LinearGradient(
+                                        0, 0, 0, 1,
+                                        [
+                                            {offset: 0, color: '#83bff6'},
+                                            {offset: 0.5, color: '#188df0'},
+                                            {offset: 1, color: '#188df0'}
+                                        ]
+                                    )
+                                },
+                                emphasis: {
+                                    color: new this.$echarts.graphic.LinearGradient(
+                                        0, 0, 0, 1,
+                                        [
+                                            {offset: 0, color: '#2378f7'},
+                                            {offset: 0.7, color: '#2378f7'},
+                                            {offset: 1, color: '#83bff6'}
+                                        ]
+                                    )
+                                }
+                            },
+                            data: data
+                        }
+                    ]
+                });
+                var zoomSize = 6;
+                myChart.on('click', function (params) {
+                    console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)]);
+                    myChart.dispatchAction({
+                        type: 'dataZoom',
+                        startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
+                        endValue: dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
+                    });
                 });
             }
         }
